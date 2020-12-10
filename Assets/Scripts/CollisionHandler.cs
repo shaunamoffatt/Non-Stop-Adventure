263c6e11957e0d06f458bdfe830280c0177edbdb
shaunamoffatt <shaunamoffatt@gmail.com>
2020-12-10 19:37:35 +0000

    implemented health, bloddparticles and healthbar reducing

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -8,20 +8,21 @@ public class CollisionHandler : MonoBehaviour
 {
     [SerializeField] GameObject bloodParticles;
     [SerializeField] float loadDelay = 0.75f;
+    [SerializeField]public HealthBar healthbar;
+    [SerializeField] int health = 5;
 
     private const string terrainName = "ForestTerrain";
     private void OnTriggerEnter(Collider collision)
     {
         Debug.Log(collision.gameObject.name.ToString());
         //SendMessage("GameObjectCollide");
-        if (collision.gameObject.name == terrainName)
-        {
-                return;
-        }
+   
         RunDeathSequence();
 
-        Invoke("ReloadScene", loadDelay);
+        //Invoke("ReloadScene", loadDelay);
     }
+
+
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -31,7 +32,14 @@ public class CollisionHandler : MonoBehaviour
 
     private void RunDeathSequence()
     {
+        Debug.Log("RunDeathSequence");
         bloodParticles.SetActive(true);
+        healthbar.SetHealth(health--);  
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        bloodParticles.SetActive(false);
     }
 
     private void ReloadScene()
---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider healthbar;
+    public Gradient gradient;
+    public Image image;
+
+    private void Start()
+    {
+        healthbar = GetComponent<Slider>();
+        
+    }
+
+    public void SetMaxHealth(int health)
+    {
+        if (image == null)
+        {
+            image = gameObject.GetComponentInChildren<Image>();
+        }
+        healthbar.maxValue = health;
+        healthbar.value = health;
+        image.color = gradient.Evaluate(1f);
+    }
+
+    public void SetHealth(int health)
+    {
+        if(image == null)
+        {
+            image = gameObject.GetComponentInChildren<Image>();
+        }
+        healthbar.value = health;
+
+        image.color = gradient.Evaluate(healthbar.normalizedValue);
+    }
+}
