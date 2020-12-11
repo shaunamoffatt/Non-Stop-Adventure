c55e850410319426ad28412cbc60af30b9b030d0
shaunamoffatt <shaunamoffatt@gmail.com>
2020-12-11 18:38:52 +0000

    simple jump animation added, collectable added

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    [SerializeField] float rotateSpeed = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, rotateSpeed, 0),Space.World);
+    }
+}
